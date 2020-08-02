42111ced8e1f78a6eb7bc8ec6318a2a9fe7f5272
kevinfcc26 <kevinfcc26@gmail.com>
2020-08-02 10:14:32 -0500

    add * in app.get of server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/FrontTesis'));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/FrontTesis/index.html'));
 });
 
